4685e21f1be8430f0084b1e2a463c46ded5ccaac
Shashwat Kathuria <shashwatkathuria@yahoo.co.in>
2020-05-06 15:27:46 +0530

    Added config info.

---
+++ js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'us-east-1_YuG97ybVT',
+        userPoolClientId: '4g310sh0o66trhd2mgh2988ss',
+        region: 'us-east-1'
+    },
+    api: {
+        invokeUrl: 'https://m6317hccp3.execute-api.us-east-1.amazonaws.com/prod'
+    }
+};
